e36fc0c11e106f548dcdbcd6e8479616e9d045bc
Johannes Lange <johannes.lange@uni-hamburg.de>
2022-11-14 11:36:17 +0100

    trying no-cert lfn lookup
    stolen from Daniel
    https://github.com/uhh-cms/mttbar/commit/3e6e987ca92404b31133657283410e80a8704c6f

--- setup.sh
+++ setup.sh
@@ -144,7 +144,9 @@ setup_topmass() {
     #
 
     export LAW_HOME="${TM_BASE}/.law"
-    export LAW_CONFIG_FILE="${TM_BASE}/law.cfg"
+    # export LAW_CONFIG_FILE="${TM_BASE}/law.cfg"
+    export LAW_CONFIG_FILE="${TM_BASE}/law.nocert.cfg"
+
 
     if which law &> /dev/null; then
         # source law's bash completion scipt
