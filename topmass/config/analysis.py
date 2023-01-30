f97d721f238d1fef739050168376e9956fb4943a
Johannes Lange <johannes.lange@uni-hamburg.de>
2023-01-30 10:50:05 +0100

    drell-yan HT-binned samples

--- topmass/config/analysis.py
+++ topmass/config/analysis.py
@@ -87,6 +87,12 @@ stylize_processes(cfg)
 dataset_names = [
     "tt_dl_powheg",
     "w_lnu_madgraph",
+    "dy_lep_m50_ht200to400_madgraph",
+    "dy_lep_m50_ht400to600_madgraph",
+    "dy_lep_m50_ht600to800_madgraph",
+    "dy_lep_m50_ht800to1200_madgraph",
+    "dy_lep_m50_ht1200to2500_madgraph",
+    "dy_lep_m50_ht2500_madgraph",
 ]
 """
 # data
