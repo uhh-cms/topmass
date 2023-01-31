b4ce9e4d9e6a9d367fe6fcdd54461f8fd1b54fde
Marcel R <github.riga@icloud.com>
2023-01-31 10:47:41 +0100

    Update cf, use producers from cms scopes.

--- topmass/production/features.py
+++ topmass/production/features.py
@@ -6,7 +6,7 @@ Column production methods related to higher-level features.
 
 from columnflow.production import Producer, producer
 from columnflow.production.categories import category_ids
-from columnflow.production.mc_weight import mc_weight
+from columnflow.production.cms.mc_weight import mc_weight
 from columnflow.util import maybe_import
 from columnflow.columnar_util import EMPTY_FLOAT, Route, set_ak_column
 
@@ -37,13 +37,13 @@ def features(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     events = set_ak_column(events, "n_bjet", ak.num(events.Bjet.pt, axis=1))
     events = set_ak_column(events, "n_electron", ak.num(events.Electron.pt, axis=1))
     events = set_ak_column(events, "n_muon", ak.num(events.Muon.pt, axis=1))
-    
+
     lepton_pt = ak.concatenate((events.Muon.pt,events.Electron.pt), axis=1)
     lepton_pt = ak.sort(lepton_pt,axis=-1, ascending=False)
-    
+
     events = set_ak_column(events, "lepton_pt", lepton_pt)
 
-    
+
     return events
 
 
@@ -69,7 +69,7 @@ def lb_features(self: Producer, events: ak.Array, **kwargs) -> ak.Array:
     events["Electron"] = ak.with_name(events.Electron, "PtEtaPhiMLorentzVector")
     events = ak.Array(events, behavior=coffea.nanoevents.methods.nanoaod.behavior)
     events["Muon"] = ak.with_name(events.Muon, "PtEtaPhiMLorentzVector")
-    
+
     leptons = ak.concatenate((events.Electron, events.Muon), axis=1)
 
     if ak.any(ak.num(events.Bjet, axis=-1) != 2):
