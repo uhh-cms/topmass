568cfe9b0fbb5d693f0b9dbce0ce5297c5ce8631
Tom Paul Davids <davidsto@naf-cms16.desy.de>
2024-01-08 11:56:58 +0100

    third commit

--- alljets/selection/example.py
+++ alljets/selection/example.py
@@ -63,7 +63,7 @@ def jet_selection(
 ) -> tuple[ak.Array, SelectionResult]:
     # example jet selection: at least six jets, lowest jet at least 40 GeV and H_T > 450 GeV
     jet_mask = ((events.Jet.pt >= 40.0) & (abs(events.Jet.eta) < 2.4) & (ak.sum(events.Jet.pt, axis=1) >= 450))
-    jet_sel = ak.sum(jet_mask, axis=1) >= 6
+    jet_sel = ak.sum(jet_mask, axis=1) == 6
 
     # pt sorted indices
     # indices = ak.argsort(events.Jet.pt, axis=-1, ascending=False)
@@ -93,6 +93,29 @@ def jet_selection(
         },
     )
 
+
+# @fh_jet_selection.init
+# def fh_jet_selection.init(self: Selector) -> None:
+#     year = self.config_inst.campaign.x.year
+#
+#     # NOTE: the none will not be overwritten later when doing this...
+#     # self.jet_trigger = None
+#
+#     # Jet pt thresholds (if not set manually) based on year (1 pt above trigger threshold)
+#     # When lepton pt thresholds are set manually, don't use any trigger
+#     if not self.jet_pt:
+#         self.jet_pt = {2016: 31, 2017: 33, 2018: 33}[year]
+#
+#         # Trigger choice based on year of data-taking (for now: only single trigger)
+#         self.jet_trigger = {
+#             2016: "HLT_PFHT400_SixJet30_DoubleBTagCSV_p056",
+# or "HLT_PFHT450_SixJet40_BTagCSV_p056")
+#             2017: "HLT_PFHT380_SixPFJet32_DoublePFBTagCSV_2p2",
+# or "HLT_PFHT380_SixPFJet32_DoublePFBTagDeepCSV_2p2", "HLT_PFHT430_SixPFJet40_PFBTagCSV_1p5")
+#             2018: "HLT_PFHT400_SixPFJet32_DoublePFBTagDeepCSV_2p94",
+# or "HLT_PFHT450_SixPFJet36_PFBTagDeepCSV_1p59")
+#         }[year]
+#         self.uses.add(f"HLT.{self.jet_trigger}")
 
 #
 # exposed selectors
