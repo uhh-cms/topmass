931fcdec2389be467cd6261ec6946a127f64708d
LennertGriesing <lennert.griesing@gmail.com>
2026-07-12 08:37:14 +0200

    Fix typo in default selector step group

--- alljets/config/configs_aj.py
+++ alljets/config/configs_aj.py
@@ -260,7 +260,7 @@ def add_config(
     # (used in cutflow tasks)
     cfg.x.selector_step_groups = {
         "cutflow_sig": ["json", "met_filter", "pv", "Trigger", "Lepton_Veto", "HT", "jet", "BTag", "LeadingSix2BTag"],
-        "default": ["json", "met_filter", "pv", "SingalOrBkgTrigger",
+        "default": ["json", "met_filter", "pv", "SignalOrBkgTrigger",
                     "Lepton_Veto", "HT", "jet", "BTag", "LeadingSix2BTag"],
         "ht_trigger": ["json", "met_filter", "pv", "BaseTrigger", "Lepton_Veto", "jet", "SixJets", "BTag"],
         "trigjet6_pt": ["json", "met_filter", "pv", "BaseTrigger", "Lepton_Veto", "HT", "BTag"],
