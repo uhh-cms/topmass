052403567d5941789d47844177af98f0086c106c
Hartmut Stadie <hartmut.stadie@cern.ch>
2026-07-17 16:29:06 +0200

    new lumi correlation scheme

--- alljets/config/configs_aj.py
+++ alljets/config/configs_aj.py
@@ -332,20 +332,23 @@ def add_config(
     # https://cms-talk.web.cern.ch/t/luminosity-in-run2023c/116859/2
     # Run3 Lumis can be calculated with brilcalc tool https://twiki.cern.ch/twiki/bin/view/CMS/BrilcalcQuickStart?rev=15
     # CClub computed this already: https://gitlab.cern.ch/cclubbtautau/AnalysisCore/-/issues/49
+    # Using the new correlation scheme from https://twiki.cern.ch/twiki/bin/viewauth/CMS/LumiRecommendationsRun2?extralog=-%20caching%20topic#Luminosity_for_pp_13_TeV_data_20
     if year == 2016 and campaign.has_tag("HIPM"):
         cfg.x.luminosity = Number(
             19_500,
             {
-                "lumi_13TeV_2016": 0.01j,
-                "lumi_13TeV_correlated": 0.006j,
+                "lumi_13TeV_1516": 0.0118j,
+                "lumi_13TeV_151617": 0.0004j,
+                "lumi_13TeV_15161718": 0.0035j,
             },
         )
     elif year == 2016 and campaign.has_tag("notHIPM"):
         cfg.x.luminosity = Number(
             16_800,
             {
-                "lumi_13TeV_2016": 0.01j,
-                "lumi_13TeV_correlated": 0.006j,
+                "lumi_13TeV_1516": 0.0118j,
+                "lumi_13TeV_151617": 0.0004j,
+                "lumi_13TeV_15161718": 0.0035j,
             },
         )
     elif year == 2017:
@@ -354,9 +357,8 @@ def add_config(
         cfg.x.luminosity = Number(
             37_186,
             {
-                "lumi_13TeV_2017": 0.02j,
-                "lumi_13TeV_1718": 0.006j,
-                "lumi_13TeV_correlated": 0.009j,
+                "lumi_13TeV_151617" : 0.0055j,
+                "lumi_13TeV_15161718": 0.0061j,
             },
         )
     elif year == 2018:
@@ -366,9 +368,7 @@ def add_config(
         cfg.x.luminosity = Number(
             59_557,
             {
-                "lumi_13TeV_2017": 0.015j,
-                "lumi_13TeV_1718": 0.002j,
-                "lumi_13TeV_correlated": 0.02j,
+                "lumi_13TeV_15161718": 0.0084j,
             },
         )
     # minimum bias cross section in mb (milli) for creating PU weights, values from
